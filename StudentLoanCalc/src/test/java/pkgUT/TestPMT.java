d7e85a98998d01037d31a1fa5f2838f7f77af83b
CISC181 <bgibbons@udel.edu>
2021-12-02 12:22:00 -0500

    Update TestPMT.java

--- StudentLoanCalc/src/test/java/pkgUT/TestPMT.java
+++ StudentLoanCalc/src/test/java/pkgUT/TestPMT.java
@@ -1,13 +1,13 @@
 package pkgUT;
 
+
 import org.apache.poi.ss.formula.functions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Collections;
-import pkgLogic.Loan;
-import java.time.LocalDate;
+
 import org.junit.jupiter.api.Test;
 
 import pkgLogic.Loan;
@@ -16,32 +16,30 @@ public class TestPMT {
 
 	@Test
 	public void TestPMT() {
-
-		// PMT is a standard function included in apache POI.
-		// For a given r (rate), n (number of payments), p (present value), f (future
-		// value), t (how compounding is applied)
-		// this function will determine payment
-
-		// This is an example with known values
-		// PMT returns with negative values (this is typical accounting).
-
+		
+		//	PMT is a standard function included in apache POI.
+		//	For a given r (rate), n (number of payments), p (present value), f (future value), t (how compounding is applied)
+		//	this function will determine payment
+		
+		//	This is an example with known values
+		//	PMT returns with negative values (this is typical accounting).  
+		
 		double PMT;
 		double r = 0.07 / 12;
 		double n = 20 * 12;
 		double p = 150000;
 		double f = 0;
 		boolean t = false;
-		PMT = Math.abs(FinanceLib.pmt(r, n, p, f, t));
-		double PMTExpected = 1162.95;
+		PMT = Math.abs(FinanceLib.pmt(r, n, p, f, t));		
+		double PMTExpected = 1162.95;		
 		assertEquals(PMTExpected, PMT, 0.01);
 	}
-
 	@Test
 	public void TestLoanWithNoExtraPayment() {
 		
 		//This unit test should work with the values given
 		double dLoanAmount = 50000;
-		double dInterestRate = 7.0;
+		double dInterestRate = 0.07;
 		int iNbrOfYears = 20;
 		LocalDate localDate = LocalDate.now();
 		double dAdditionalPayment = 0;
@@ -53,11 +51,7 @@ public class TestPMT {
 		assertEquals(loan.getTotalPayments(), 93033.62, 0.01);
 		assertEquals(loan.getTotalInterest(), 43035.87, 0.01);
 	}
-	
-	@Test
-	public void TestLoanWithExtraPayment() {
-		//TODO: Build me a unit test and test extra payments.
-		
-	}
-
 }
+
+ 
+
